1a44f1c6ff82b2a4670a9040e7f7e48b578a6516
Veronica <verociolfi@gmail.com>
2022-07-23 18:53:52 -0300

    inclusao de modulo pages e componente da pagina de listagem de herois

---
+++ src/app/pages/list-heroes/list-heroes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-heroes',
+  templateUrl: './list-heroes.component.html',
+  styleUrls: ['./list-heroes.component.scss']
+})
+export class ListHeroesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListHeroesComponent } from './list-heroes/list-heroes.component';
+
+
+
+@NgModule({
+  declarations: [
+    ListHeroesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PagesModule { }
